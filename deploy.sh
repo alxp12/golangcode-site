4f89f64c0df6a51b202c24245e9fdf6746dfb6d4
Edd Turtle <eddturtle@gmail.com>
2017-08-20 12:00:59 +0100

    Convert project to sass (from css) and minify on deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Build Assets
+sass themes/site-theme/static/css/main.scss:themes/site-theme/static/css/style.css --style compressed
+
+# Build Content
 hugo
 
+# Push Content
 s3deploy -source=public/ -region=eu-west-1 -bucket=golangcode.com
