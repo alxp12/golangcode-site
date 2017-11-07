e1d4a430ba1dce4b478229d478c3c5d50dba4dbb
Edd Turtle <eddturtle@gmail.com>
2017-11-07 22:23:43 +0000

    Sprinkle some love on the deploy output :yellow_heart:

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-# Build Assets
+echo "- Build Assets"
 sass themes/site-theme/static/css/main.scss:themes/site-theme/static/css/style.css --style compressed --sourcemap=none
 
-# Build Content
-hugo
+echo "- Run Hugo"
+hugo --quiet
 
-# Push Content
+echo "- Upload Files"
 s3deploy -source=public/ -region=eu-west-1 -bucket=golangcode.com
+
+echo "- Upload Complete :) Have a super day"
