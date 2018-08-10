092de8decd86d29b34d98fc6a398ff07b4173c5a
Edd Turtle <edd@designedbyaturtle.co.uk>
2018-08-10 21:15:12 +0100

    Improve deploy script to report failed upload + be quieter :see_no_evil:

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-echo "- Build Assets"
+echo "- Building Assets (SASS)"
 sass themes/site-theme/static/css/main.scss:themes/site-theme/static/css/style.css --style compressed --sourcemap=none
 
-echo "- Run Hugo"
+echo "- Running Hugo"
 hugo --quiet
 
-echo "- Upload Files"
-s3deploy -source=public/ -region=eu-west-1 -bucket=golangcode.com
+echo "- Upload Files to S3..."
+s3deploy -source=public/ -region=eu-west-1 -bucket=golangcode.com -quiet
 
-echo "- Upload Complete :) Have a super day"
+if [ $? -eq 0 ]; then
+	echo "- Upload Complete :) Have a super day"
+else
+	echo "- Upload Failed :("
+fi
