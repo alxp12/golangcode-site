26e4e660606504568d7b58449b862a7443e4c6fe
Edd Turtle <edd@designedbyaturtle.co.uk>
2019-06-01 08:53:29 +0100

    Improve compress script & run to shrink imgz :shower:

--- compress.sh
+++ compress.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+if ! [ -x "$(command -v optipng)" ]; then
+  echo 'Error: optipng is not installed.' >&2
+  exit 1
+fi
+
 find static/ -name '*.png' -print0 | xargs -0 optipng -nc -nb -o7
