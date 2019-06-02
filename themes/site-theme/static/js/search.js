c8dec4fb4dace305906c6ccef181fc47cff34e4b
Edd Turtle <edd@designedbyaturtle.co.uk>
2019-06-02 09:46:04 +0100

    beautify search js code :sunglasses:

--- themes/site-theme/static/js/search.js
+++ themes/site-theme/static/js/search.js
@@ -1,19 +1,19 @@
-summaryInclude=200;
+summaryInclude = 200;
+
 var fuseOptions = {
-  shouldSort: true,
-  includeMatches: true,
-  threshold: 0.0,
-  tokenize:true,
-  location: 0,
-  distance: 100,
-  maxPatternLength: 32,
-  minMatchCharLength: 1,
-  keys: [
-    {name:"title",weight:0.8},
-    {name:"contents",weight:0.5},
-    {name:"tags",weight:0.3},
-    {name:"categories",weight:0.3}
-  ]
+    shouldSort: true,
+    includeMatches: true,
+    threshold: 0.0,
+    tokenize: true,
+    location: 0,
+    distance: 100,
+    maxPatternLength: 32,
+    minMatchCharLength: 1,
+    keys: [
+        { name: "title", weight: 0.8 },
+        { name: "contents", weight: 0.5 },
+        { name: "tags", weight: 0.3 },
+    ]
 };
 
 
@@ -23,9 +23,9 @@ if (searchQuery) {
     executeSearch(searchQuery);
     // Log
     if (typeof ga === 'function') {
-        ga('send', { 
-            hitType: 'event', 
-            eventCategory: 'search', 
+        ga('send', {
+            hitType: 'event',
+            eventCategory: 'search',
             eventAction: 'query',
             eventLabel: searchQuery,
         });
@@ -35,66 +35,65 @@ if (searchQuery) {
 }
 
 
-
-function executeSearch(searchQuery){
-  $.getJSON( "/index.json", function( data ) {
-    var pages = data;
-    var fuse = new Fuse(pages, fuseOptions);
-    var result = fuse.search(searchQuery);
-    if(result.length > 0){
-      populateResults(result);
-    }else{
-      $('.search-no-results').show();
-    }
-  });
+function executeSearch(searchQuery) {
+    $.getJSON("/index.json", function(data) {
+        var pages = data;
+        var fuse = new Fuse(pages, fuseOptions);
+        var result = fuse.search(searchQuery);
+        if (result.length > 0) {
+            populateResults(result);
+        } else {
+            $('.search-no-results').show();
+        }
+    });
 }
 
-function populateResults(result){
-  $.each(result,function(key,value){
-    var contents= value.item.contents;
-    var snippet = "";
-    var snippetHighlights=[];
-    var tags =[];
-    if( fuseOptions.tokenize ){
-      snippetHighlights.push(searchQuery);
-    }else{
-      $.each(value.matches,function(matchKey,mvalue){
-        if(mvalue.key == "tags" || mvalue.key == "categories" ){
-          snippetHighlights.push(mvalue.value);
-        }else if(mvalue.key == "contents"){
-          start = mvalue.indices[0][0]-summaryInclude>0?mvalue.indices[0][0]-summaryInclude:0;
-          end = mvalue.indices[0][1]+summaryInclude<contents.length?mvalue.indices[0][1]+summaryInclude:contents.length;
-          snippet += contents.substring(start,end);
-          snippetHighlights.push(mvalue.value.substring(mvalue.indices[0][0],mvalue.indices[0][1]-mvalue.indices[0][0]+1));
+function populateResults(result) {
+    $.each(result, function(key, value) {
+        var contents = value.item.contents;
+        var snippet = "";
+        var snippetHighlights = [];
+        var tags = [];
+        if (fuseOptions.tokenize) {
+            snippetHighlights.push(searchQuery);
+        } else {
+            $.each(value.matches, function(matchKey, mvalue) {
+                if (mvalue.key == "tags" || mvalue.key == "categories") {
+                    snippetHighlights.push(mvalue.value);
+                } else if (mvalue.key == "contents") {
+                    start = mvalue.indices[0][0] - summaryInclude > 0 ? mvalue.indices[0][0] - summaryInclude : 0;
+                    end = mvalue.indices[0][1] + summaryInclude < contents.length ? mvalue.indices[0][1] + summaryInclude : contents.length;
+                    snippet += contents.substring(start, end);
+                    snippetHighlights.push(mvalue.value.substring(mvalue.indices[0][0], mvalue.indices[0][1] - mvalue.indices[0][0] + 1));
+                }
+            });
         }
-      });
-    }
 
-    if(snippet.length<1){
-      snippet += contents.substring(0,summaryInclude*2);
-    }
+        if (snippet.length < 1) {
+            snippet += contents.substring(0, summaryInclude * 2);
+        }
 
-    snippet += '&hellip;';
+        snippet += '&hellip;';
 
-    //pull template from hugo templarte definition
-    var templateDefinition = $('#search-result-template').html();
-    //replace values
-    var output = render(templateDefinition, {
-        key: key,
-        title: value.item.title,
-        img: (key < 3 ? value.item.img : null),
-        link: value.item.permalink,
-        tags: value.item.tags.join(', '),
-        categories: value.item.categories,
-        snippet: snippet
+        //pull template from hugo templarte definition
+        var templateDefinition = $('#search-result-template').html();
+        //replace values
+        var output = render(templateDefinition, {
+            key: key,
+            title: value.item.title,
+            img: (key < 3 ? value.item.img : null),
+            link: value.item.permalink,
+            tags: value.item.tags.join(', '),
+            categories: value.item.categories,
+            snippet: snippet
+        });
+        $('#search-results').append(output);
+
+        $.each(snippetHighlights, function(snipkey, snipvalue) {
+            $("#summary-" + key).mark(snipvalue);
+        });
+
     });
-    $('#search-results').append(output);
-
-    $.each(snippetHighlights,function(snipkey,snipvalue){
-      $("#summary-"+key).mark(snipvalue);
-    });
-
-  });
 }
 
 function param(name) {
@@ -102,27 +101,26 @@ function param(name) {
 }
 
 function render(templateString, data) {
-  var conditionalMatches,conditionalPattern,copy;
-  conditionalPattern = /\$\{\s*isset ([a-zA-Z]*) \s*\}(.*)\$\{\s*end\s*}/g;
-  //since loop below depends on re.lastInxdex, we use a copy to capture any manipulations whilst inside the loop
-  copy = templateString;
-  while ((conditionalMatches = conditionalPattern.exec(templateString)) !== null) {
-    if(data[conditionalMatches[1]]){
-      //valid key, remove conditionals, leave contents.
-      copy = copy.replace(conditionalMatches[0],conditionalMatches[2]);
-    }else{
-      //not valid, remove entire section
-      copy = copy.replace(conditionalMatches[0],'');
+    var conditionalMatches, conditionalPattern, copy;
+    conditionalPattern = /\$\{\s*isset ([a-zA-Z]*) \s*\}(.*)\$\{\s*end\s*}/g;
+    //since loop below depends on re.lastInxdex, we use a copy to capture any manipulations whilst inside the loop
+    copy = templateString;
+    while ((conditionalMatches = conditionalPattern.exec(templateString)) !== null) {
+        if (data[conditionalMatches[1]]) {
+            //valid key, remove conditionals, leave contents.
+            copy = copy.replace(conditionalMatches[0], conditionalMatches[2]);
+        } else {
+            //not valid, remove entire section
+            copy = copy.replace(conditionalMatches[0], '');
+        }
     }
-  }
-  templateString = copy;
-  //now any conditionals removed we can do simple substitution
-  var key, find, re;
-  for (key in data) {
-    find = '\\$\\{\\s*' + key + '\\s*\\}';
-    re = new RegExp(find, 'g');
-    templateString = templateString.replace(re, data[key]);
-  }
-  return templateString;
+    templateString = copy;
+    //now any conditionals removed we can do simple substitution
+    var key, find, re;
+    for (key in data) {
+        find = '\\$\\{\\s*' + key + '\\s*\\}';
+        re = new RegExp(find, 'g');
+        templateString = templateString.replace(re, data[key]);
+    }
+    return templateString;
 }
-
