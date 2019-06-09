d161e3f003a3a6efe3bf6d075c6c180d085ea9e0
Edd Turtle <edd@designedbyaturtle.co.uk>
2019-06-09 11:55:17 +0100

    Project re-arrange - use hugo to build sass & improve image compression :feet:

--- compress.sh
+++ compress.sh
@@ -1,8 +1,29 @@
 #!/usr/bin/env bash
 
+# PNG
 if ! [ -x "$(command -v optipng)" ]; then
-  echo 'Error: optipng is not installed.' >&2
-  exit 1
+    echo 'Skip: optipng is not installed.'
+else
+    find static/ -name '*.png' -print0 | xargs -0 optipng -nc -nb -o7
+    find themes/ -name '*.png' -print0 | xargs -0 optipng -nc -nb -o7   
 fi
 
-find static/ -name '*.png' -print0 | xargs -0 optipng -nc -nb -o7
+# PNG 2
+if ! [ -x "$(command -v pngcrush)" ]; then
+    echo 'Skip: pngcrush is not installed.'
+else
+    for png in `find $1 -name "*.png"`;
+    do
+        echo "crushing $png"    
+        pngcrush -brute "$png" temp.png
+        mv -f temp.png $png
+    done;
+fi
+
+# JPG
+if ! [ -x "$(command -v jpegoptim)" ]; then
+    echo 'Skip: jpegoptim is not installed.'
+else
+    find static/ -type f -name "*.jpg" -o -name "*.JPG" | xargs jpegoptim -f --strip-all
+    find themes/ -type f -name "*.jpg" -o -name "*.JPG" | xargs jpegoptim -f --strip-all    
+fi
